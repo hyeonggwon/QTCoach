e9413b83b3af935947f7f987d7f5d7c65b8cb192
hyeonggwon <asdhjk98@naver.com>
2025-08-14 04:09:22 +0900

    configuration commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:qt_coach/main.dart';
+import 'package:qt_coach/layout.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
